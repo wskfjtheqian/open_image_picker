3e759473afd6a662806104afe4813fc0c9866c99
heqian <wskfjtheqian@163.com>
2021-03-15 09:21:27 +0800

    修改Web 不能打开图片的BUG

--- lib/src/open_image_picker_web.dart
+++ lib/src/open_image_picker_web.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:core';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
+
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html';
 
@@ -9,7 +10,7 @@ import 'package:flutter/services.dart';
 import 'open_image_picker_pc.dart' as pc;
 
 Future<ui.Image> zoomImage(ui.Image image, double maxWidth, double maxHeight) {
-  if (image.runtimeType.toString() == "CkImage") {
+  if (!(image is ImageElement)) {
     return pc.zoomImage(image, maxWidth, maxHeight);
   }
   if (null == maxWidth && null == maxHeight) {
